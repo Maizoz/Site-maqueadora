c54f115af43e0dada33a7fa118c014dc1bf37984
Maizoz <matheus13lm@gmail.com>
2023-11-28 19:57:05 -0300

    aaa

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const card = document.querySelector(".myCard");
     const button = document.querySelector(".button");
     const button1 = document.querySelector(".button1");
+
   
     button.addEventListener("click", function() {
       card.classList.add("active");
@@ -11,4 +12,19 @@ document.addEventListener("DOMContentLoaded", function() {
       card.classList.remove("active");
     });
   });
+  document.addEventListener("DOMContentLoaded", function() {
+    const card = document.querySelector(".myCard2");
+    const button2 = document.querySelector(".button2");
+    const button3 = document.querySelector(".button3");
+
+  
+    button2.addEventListener("click", function() {
+      card.classList.add("active");
+    });
+  
+    button3.addEventListener("click", function() {
+      card.classList.remove("active");
+    });
+  });
+  
   
